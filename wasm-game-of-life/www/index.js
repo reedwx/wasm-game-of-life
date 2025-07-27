bb83a75b20bfc45e3616d28ea2219a3b00c65eb2
reedwx <jrp375@cornell.edu>
2025-07-27 00:14:38 -0700

    added game of life logic and js runtime capability

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "../pkg/wasm_game_of_life.js";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+
